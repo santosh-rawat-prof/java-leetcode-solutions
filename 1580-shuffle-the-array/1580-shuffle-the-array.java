c63cdeff2a17a3fcc94d2e239e532116186de5ff
Santosh Kumar Raut <147261516+santosh-rawat-prof@users.noreply.github.com>
2024-11-05 19:12:46 +0530

    Time: 0 ms (100%), Space: 44.8 MB (27.73%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.java
+++ 1580-shuffle-the-array/1580-shuffle-the-array.java
@@ -1,19 +1,12 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        int len = 2*n;
-        int[] shuffledArray = new int[len];
+        int[] shuffledArray = new int[2 * n];
 
-        int p1 = 0, p2 = n;
-
-        for(int i = 0; i < len; i++) {
-            if(i % 2 == 0) {
-                shuffledArray[i] = nums[p1];
-                p1++;
-            } else {
-                shuffledArray[i] = nums[p2];
-                p2++;
-            }
+        for(int i = 0; i < n; i++) {
+            shuffledArray[2 * i] = nums[i];
+            shuffledArray[2 * i + 1] = nums[i + n];
         }
+        
         return shuffledArray;
     }
 }
